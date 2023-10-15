ed1a0be9bceaa9f1ceea44b51636ef81aac48760
atiqur rahman <abulbasharatiqur@gmail.com>
2023-10-15 21:09:48 +0600

    Fresh Main

--- pro-stream-client/src/App.js
+++ pro-stream-client/src/App.js
@@ -3,17 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <h1>Hello World2!</h1>
-      <h1>Hello World3333!</h1>
-      <h1>New Line for branches</h1>
-      <h1>Hello World!</h1>
-      <h1>Hello World2!</h1>
-      <h1>Hello World3333!</h1>
-      <h1>Hello World!</h1>
-      <h1>Hello World2!</h1>
-      <h1>Hello World3333!</h1>
-      <h3>Atiq added a line</h3>
+      
     </div>
   );
 }
