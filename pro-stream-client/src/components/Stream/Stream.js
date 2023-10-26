e6b643e7f410816a1ad940c916f8b3536aec3081
atiqur rahman <abulbasharatiqur@gmail.com>
2023-10-26 14:05:52 +0600

    about added

--- pro-stream-client/src/components/Stream/Stream.js
+++ pro-stream-client/src/components/Stream/Stream.js
@@ -7,6 +7,7 @@ import {
   Menu,
   MenuItem,
   Paper,
+  Stack,
   TextField,
   Toolbar,
   Typography,
@@ -19,6 +20,25 @@ import AddReactionOutlinedIcon from "@mui/icons-material/AddReactionOutlined";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import SendIcon from "@mui/icons-material/Send";
+import stream1 from "../../Images/stream1.png";
+import VerifiedIcon from "@mui/icons-material/Verified";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+
+const TikTokIcon = ({ color = "#ffffff" }) => {
+  return (
+    <svg
+      fill={color}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 50 50"
+      width="25px"
+      height="25px"
+    >
+      <path d="M41,4H9C6.243,4,4,6.243,4,9v32c0,2.757,2.243,5,5,5h32c2.757,0,5-2.243,5-5V9C46,6.243,43.757,4,41,4z M37.006,22.323 c-0.227,0.021-0.457,0.035-0.69,0.035c-2.623,0-4.928-1.349-6.269-3.388c0,5.349,0,11.435,0,11.537c0,4.709-3.818,8.527-8.527,8.527 s-8.527-3.818-8.527-8.527s3.818-8.527,8.527-8.527c0.178,0,0.352,0.016,0.527,0.027v4.202c-0.175-0.021-0.347-0.053-0.527-0.053 c-2.404,0-4.352,1.948-4.352,4.352s1.948,4.352,4.352,4.352s4.527-1.894,4.527-4.298c0-0.095,0.042-19.594,0.042-19.594h4.016 c0.378,3.591,3.277,6.425,6.901,6.685V22.323z" />
+    </svg>
+  );
+};
 
 const Stream = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -43,73 +63,136 @@ const Stream = () => {
               allowFullScreen
             ></iframe>
           </Paper>
-          <Grid container alignItems="center" spacing={2}>
-            <Grid item>
-              <Avatar
-                sx={{ height: "70px", width: "70px" }}
-                src="https://i.ibb.co/xHhvpZj/people-1.png"
-              />
+          <Stack direction="column" spacing={1}>
+            <Grid container alignItems="center" spacing={1}>
+              <Grid item>
+                <Avatar
+                  sx={{ height: "70px", width: "70px" }}
+                  src="https://i.ibb.co/xHhvpZj/people-1.png"
+                />
+              </Grid>
+              <Grid item>
+                <Typography variant="h5" sx={{ color: "white" }}>
+                  Atiqur Rahman
+                </Typography>
+                <Typography variant="body2" sx={{ color: "white" }}>
+                  [Drops] New battle item inventory
+                </Typography>
+                <Typography variant="h6" sx={{ color: "white" }}>
+                  Category name
+                </Typography>
+                <Typography variant="body" sx={{ color: "white" }}>
+                  tag1
+                </Typography>{" "}
+                <Typography variant="body" sx={{ color: "white" }}>
+                  tag2
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  sx={{ marginRight: "5px" }}
+                  startIcon={<AddReactionOutlinedIcon />}
+                >
+                  React
+                </Button>
+                <Button
+                  variant="contained"
+                  sx={{ marginRight: "5px" }}
+                  startIcon={<FavoriteBorderIcon />}
+                >
+                  Follow
+                </Button>
+                <Button variant="contained" startIcon={<StarBorderIcon />}>
+                  Subscribe
+                </Button>
+                <IconButton
+                  aria-label="more"
+                  id="basic-button"
+                  aria-controls={open ? "basic-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  onClick={handleClick}
+                >
+                  <MoreVert sx={{ color: "white" }} />
+                </IconButton>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                  }}
+                  sx={{ marginTop: "5px" }}
+                >
+                  <MenuItem onClick={handleClose}>Report Live Stream</MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    Report Someting Else
+                  </MenuItem>
+                </Menu>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Typography variant="h4" sx={{ color: "white" }}>
-                Atiqur Rahman
+            <Stack padding={2}>
+              <Typography variant="h5" color="white">
+                About Atiqur Rahman <VerifiedIcon />
               </Typography>
-              <Typography variant="body" sx={{ color: "white" }}>
-                [Drops] New battle item inventory
+              <Typography variant="body2" color="white">
+                <b>5.4M</b> followers
               </Typography>
-              <Typography variant="h6" sx={{ color: "white" }}>
-                Category name
+              <br />
+              <Typography variant="text2" color="white">
+                Welcome to the Riot Games channel, home of LoL Esports and other
+                livestreams related to our games. For LoL Esports broadcasts,
+                schedules, standings and advanced viewing features, head to
+                http://lolesports.com.
               </Typography>
-              <Typography variant="body" sx={{ color: "white" }}>
-                tag1
-              </Typography>{" "}
-              <Typography variant="body" sx={{ color: "white" }}>
-                tag2
-              </Typography>
-            </Grid>
-            <Grid item>
+            </Stack>
+            <Stack direction="row" spacing={2} padding={2}>
               <Button
-                variant="contained"
-                sx={{ marginRight: "5px" }}
-                startIcon={<AddReactionOutlinedIcon />}
+                variant="outlined"
+                sx={{ color: "white" }}
+                startIcon={<FacebookIcon />}
               >
-                React
+                Facebook
               </Button>
               <Button
-                variant="contained"
-                sx={{ marginRight: "5px" }}
-                startIcon={<FavoriteBorderIcon />}
+                variant="outlined"
+                sx={{ color: "white" }}
+                startIcon={<YouTubeIcon />}
               >
-                Follow
+                YouTube
               </Button>
-              <Button variant="contained" startIcon={<StarBorderIcon />}>
-                Subscribe
+              <Button
+                variant="outlined"
+                sx={{ color: "white" }}
+                startIcon={<InstagramIcon />}
+              >
+                Instagram
               </Button>
-              <IconButton
-                aria-label="more"
-                id="basic-button"
-                aria-controls={open ? "basic-menu" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-                onClick={handleClick}
+              <Button
+                variant="outlined"
+                sx={{ color: "white" }}
+                startIcon={<TikTokIcon />}
               >
-                <MoreVert sx={{ color: "white" }} />
-              </IconButton>
-              <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                  "aria-labelledby": "basic-button",
-                }}
-                sx={{ marginTop: "5px" }}
-              >
-                <MenuItem onClick={handleClose}>Report Live Stream</MenuItem>
-                <MenuItem onClick={handleClose}>Report Someting Else</MenuItem>
-              </Menu>
+                TikTok
+              </Button>
+            </Stack>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <img style={{ width: "100%" }} src={stream1} alt="" />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <img style={{ width: "100%" }} src={stream1} alt="" />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <img style={{ width: "100%" }} src={stream1} alt="" />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <img style={{ width: "100%" }} src={stream1} alt="" />
+              </Grid>
             </Grid>
-          </Grid>
+          </Stack>
         </Grid>
 
         {/*  Chat Page */}
@@ -129,17 +212,25 @@ const Stream = () => {
             </Typography>
             <br />
           </Paper>
-          <Box paddingY={2} alignItems="center">
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              sx={{ background: "white", width: "400px", marginRight: "10px" }}
-            />
-            <Button
-              variant="contained"
-              endIcon={<SendIcon sx={{ width: "50px", height: "40px" }} />}
-            />
-          </Box>
+          <Grid container paddingY={2} alignItems="center" spacing={2}>
+            <Grid item xs={8}>
+              <TextField
+                id="outlined-basic"
+                variant="outlined"
+                sx={{
+                  background: "white",
+                  width: "100%",
+                  marginRight: "10px",
+                }}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <Button
+                variant="contained"
+                endIcon={<SendIcon sx={{ width: "50px", height: "40px" }} />}
+              />
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Box>
