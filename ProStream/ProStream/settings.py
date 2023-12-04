70ce0c9a16f8ab409d32bad92c31a3ccd8587d79
AlSaimun <abdullahalsaimun.2002@gmail.com>
2023-12-04 20:34:39 +0600

    delete EMAIL_HOST_USER and EMAIL_HOST_PASSWORD

--- ProStream/ProStream/settings.py
+++ ProStream/ProStream/settings.py
@@ -225,5 +225,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True 
-EMAIL_HOST_USER = "weswabhimaan@gmail.com"   #  os.environ.get('EMAIL_HOST_USER')   # use decuple for env MUST format this before deploy 
-EMAIL_HOST_PASSWORD = "vkcxnzjivwsppavr"    #os.environ.get('EMAIL_HOST_PASSWORD') #  
+EMAIL_HOST_USER = ""   #  os.environ.get('EMAIL_HOST_USER')   # use decuple for env MUST format this before deploy 
+EMAIL_HOST_PASSWORD = ""    #os.environ.get('EMAIL_HOST_PASSWORD') #  
